1c916d59c3fd6d662dfcc9cf319e9153c1d28acf
Joemel Atiga <sample@example.com>
2022-10-26 01:45:06 +0000

    Committing mapquest

---
+++ mapquest_parse-json_1.py
@@ -0,0 +1,11 @@
+import urllib.parse
+import requests
+
+main_api = "https://www.mapquestapi.com/directions/v2/route?"
+orig = "Rome, Italy"
+dest = "Frascati, Italy"  
+key = "O6jLaqNmegzE2w927AbY43VgoojQnWLb"
+
+url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest})
+json_data = requests.get(url).json()
+print(json_data)
